99253de48938be5fe17b07b2bf23eaccd6910e4a
ftosfc <ftosfc@163.com>
2021-04-30 00:30:46 +0800

    4.29

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+    {path:'/',redirect:'/login'},
+    {path: '/login', component: Login}
 
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
